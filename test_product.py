7641d8dca80899bc36ab7f149e6808badea401ed
srustinaik <srustin47@gmail.com>
2026-01-09 14:35:48 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,10 +1,11 @@
 from product import product_details
-def test_product_details():    
-    expected_output=(
-         "product id=L1001\n"
-         "product name=Laptop\n"
-         "quantity=2\n"
-         "price=75000\n"
+
+def test_product_details():
+    expected_output = (
+        "product ID:L1001\n"
+        "product Name:Laptop\n"
+        "product Quantity:2\n"
+        "product Price:75000"
     )
-  assert product_details("L1001","Laptop",2,75000) == expected_output
-  
+
+    assert product_details("L1001", "Laptop", 2, 75000) == expected_output
